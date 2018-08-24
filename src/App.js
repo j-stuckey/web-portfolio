0016b4e7b337d19cf7de37662ccbafa86851da39
James Stuckey <jstuckey@rutgers.edu>
2018-08-24 00:34:06 -0400

    finally got CSS modules working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.css';
 
 class App extends Component {
   render() {
@@ -13,6 +13,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <h3 className={styles.stuff}>stuff goes here</h3>
       </div>
     );
   }
