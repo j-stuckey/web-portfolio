f38f5e925a601df45162fd9593e16300a314d599
James Stuckey <jstuckey@rutgers.edu>
2018-08-24 00:37:15 -0400

    removed basic styling from base CRA

--- src/App.js
+++ src/App.js
@@ -5,17 +5,15 @@ import styles from './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <h3 className={styles.stuff}>stuff goes here</h3>
-      </div>
+        <React.Fragment>
+            
+            <header>
+                <nav>
+                    <button>Home</button>
+                </nav>
+            </header>
+            
+        </React.Fragment>
     );
   }
 }
