e8dbe1e2dd22fd07a891b56fd86a8166f90defb9
James Stuckey <jstuckey@rutgers.edu>
2018-09-06 23:14:54 -0400

    added my name to the page

--- src/nav.js
+++ src/nav.js
@@ -3,18 +3,23 @@ import { Link } from 'react-router-dom';
 import styles from './styles/nav.css';
 
 export default class NavBar extends React.Component {
-    navButton(){
+    navButton() {
         console.log('button pressed');
     }
 
     render() {
         return (
-            <nav className={styles.navbar} >
-                <Link to="/">Home</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/aboutme">About me</Link>
-                <Link to="/contact">Contact me</Link>
-            </nav>
+            <React.Fragment>
+                <Link to="/" className={styles.name}>
+                    James Stuckey
+                </Link>
+
+                <nav className={styles.navbar}>
+                    <Link to="/projects">Work</Link>
+                    <Link to="/aboutme">About me</Link>
+                    <Link to="/contact">Contact me</Link>
+                </nav>
+            </React.Fragment>
         );
     }
 }
