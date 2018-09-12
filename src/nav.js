e3214fbdbcdc7703ac5be5027f18c4b95b77015e
James Stuckey <jstuckey@rutgers.edu>
2018-09-12 11:32:32 -0400

    removed unused button and Link

--- src/nav.js
+++ src/nav.js
@@ -3,16 +3,12 @@ import { Link } from 'react-router-dom';
 import styles from './styles/nav.css';
 
 export default class NavBar extends React.Component {
-    navButton() {
-        console.log('button pressed');
-    }
-
     render() {
         return (
             <React.Fragment>
                 <nav className={styles.navbar}>
                     <h3 className={styles.name}>James Stuckey</h3>
-                    {/* <Link to="/">Home</Link> */}
+
                     <Link to="/projects">Work</Link>
                     <Link to="/aboutme">About me</Link>
                     <Link to="/contact">Contact me</Link>
