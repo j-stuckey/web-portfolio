a41de0969435c50ca72a71459e1fd120281e3f26
James Stuckey <jstuckey@rutgers.edu>
2018-09-14 10:36:54 -0400

    added a new line

--- src/contact.js
+++ src/contact.js
@@ -10,6 +10,7 @@ export default function ContactForm(props) {
 
             <div className={styles.box}>
 
+
                 <a href="mailto:jstuckey@rutgers.edu" className={styles.contact} target="_blank">jstuckey@rutgers.edu</a>
                 <a href="https://github.com/j-stuckey" className={styles.contact} target="_blank">Github</a>
                 <a href="https://www.linkedin.com/in/james-stuckey-5b1179b9/" className={styles.contact} target="_blank">LinkedIn</a>
