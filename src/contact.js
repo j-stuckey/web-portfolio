75737035e9efce7d2064506e0f955af2621ed5f8
James Stuckey <jstuckey@rutgers.edu>
2018-09-14 10:02:54 -0400

    changed the name of a css style

--- src/contact.js
+++ src/contact.js
@@ -10,10 +10,10 @@ export default function ContactForm(props) {
 
             <div className={styles.box}>
 
-                <a href="mailto:example@email.com" className={styles.email} target="_blank">Email</a>
-                <a href="mailto:example@email.com" className={styles.email} target="_blank">Github</a>
-                <a href="mailto:example@email.com" className={styles.email} target="_blank">LinkedIn</a>
-                <a href="mailto:example@email.com" className={styles.email} target="_blank">Resume</a>
+                <a href="mailto:example@email.com" className={styles.contact} target="_blank">Email</a>
+                <a href="mailto:example@email.com" className={styles.contact} target="_blank">Github</a>
+                <a href="mailto:example@email.com" className={styles.contact} target="_blank">LinkedIn</a>
+                <a href="mailto:example@email.com" className={styles.contact} target="_blank">Resume</a>
             </div>
         </section>
     );
