cdad37311a34a8d23f0f8f52fa471cbeb039eb7a
James Stuckey <jstuckey@rutgers.edu>
2018-09-14 14:57:58 -0400

    finished about me

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './nav';
 import Projects from './projects';
 import ContactForm from './contact';
 import LandingPage from './landing-page';
+import AboutMe from './aboutme';
 import { Route } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -18,7 +19,7 @@ class App extends Component {
                     <Route path="/" component={NavBar} />
                     <Route exact path="/" component={LandingPage} />
                     <Route exact path="/projects" component={Projects}/>
-                    
+                    <Route exact path="/aboutme" component={AboutMe} />
                     <Route exact path="/contact" component={ContactForm}/>
                 </React.Fragment>
                 
---
+++ src/aboutme.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './styles/aboutme.css';
+
+
+export default function AboutMe(props) {
+
+    return (
+        <section className={styles.section}>
+
+            <img src={require('./images/workplace-1246677_1920.jpg')}/>
+
+            <div className={styles.text}>
+                <p>Hello. I am </p><a href="https://docs.google.com/document/d/1-a2JaiRNNneWVwWytYLipJjv3-YjRqu73BylTuNB6jM/edit?usp=sharing" target="_blank" className={styles.large}>James.</a><p> I like building applications, learning new things and solving problems.</p>
+                
+
+            </div>
+        </section>
+    );
+}
