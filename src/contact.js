487f9c12e09891b76e99d7472b5ca51c1ad4b332
James Stuckey <jstuckey@rutgers.edu>
2018-09-14 10:15:09 -0400

    updated contact info

--- src/contact.js
+++ src/contact.js
@@ -10,10 +10,10 @@ export default function ContactForm(props) {
 
             <div className={styles.box}>
 
-                <a href="mailto:example@email.com" className={styles.contact} target="_blank">Email</a>
-                <a href="mailto:example@email.com" className={styles.contact} target="_blank">Github</a>
-                <a href="mailto:example@email.com" className={styles.contact} target="_blank">LinkedIn</a>
-                <a href="mailto:example@email.com" className={styles.contact} target="_blank">Resume</a>
+                <a href="mailto:jstuckey@rutgers.edu" className={styles.contact} target="_blank">jstuckey@rutgers.edu</a>
+                <a href="https://github.com/j-stuckey" className={styles.contact} target="_blank">Github</a>
+                <a href="https://www.linkedin.com/in/james-stuckey-5b1179b9/" className={styles.contact} target="_blank">LinkedIn</a>
+                {/* <a href="mailto:example@email.com" className={styles.contact} target="_blank">Resume</a> */}
             </div>
         </section>
     );
