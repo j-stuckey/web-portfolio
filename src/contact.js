f1f5d3780af4597bae00ff4e9381e7946cd7d9c4
James Stuckey <jstuckey@rutgers.edu>
2018-09-24 09:58:22 -0400

    fixed view my projects button on landing page

--- src/contact.js
+++ src/contact.js
@@ -15,6 +15,7 @@ export default function ContactForm(props) {
                 <a href="https://github.com/j-stuckey" className={styles.contact} target="_blank" rel="noopener noreferrer">Github</a>
                 <a href="https://www.linkedin.com/in/james-stuckey-5b1179b9/" className={styles.contact} target="_blank" rel="noopener noreferrer">LinkedIn</a>
                 <a href="https://docs.google.com/document/d/1-a2JaiRNNneWVwWytYLipJjv3-YjRqu73BylTuNB6jM/edit?usp=sharing" className={styles.contact} target="_blank" rel="noopener noreferrer">Resume</a>
+
             </div>
         </section>
     );
--- src/landing-page.js
+++ src/landing-page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './styles/landing-page.css';
 import portrait from './images/IMG_0727.jpg';
+import { Link } from 'react-router-dom';
 
 export default function LandingPage(props){
 
@@ -13,7 +14,7 @@ export default function LandingPage(props){
             <p className={styles.text}>I'm a software developer in the NYC Area. </p>
             <p className={styles.text}>My tech stack of choice includes Node.js, React and Redux and MongoDB but, I also have experience with Python and Ruby.</p>
             <p className={styles.text}>I love learning new things and solving problems. I'm excited to be a part of creating something amazing because the best things are created through amazing teamwork. </p>
-            <p className={styles.text}><a href="/projects" className={styles.button}>View my projects</a></p>
+            <p className={styles.text}><Link to="/projects" className={styles.button}>View my projects</Link></p>
         </section>
         
     );
