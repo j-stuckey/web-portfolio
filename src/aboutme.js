4e268644774f94d136d3749a52c0eac5adf1b30a
James Stuckey <jstuckey@rutgers.edu>
2018-09-30 14:41:47 -0400

    fixed resume

--- src/aboutme.js
+++ src/aboutme.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './styles/aboutme.css';
-
+import resume from './assets/resume.pdf';
 
 export default function AboutMe(props) {
 
@@ -10,7 +10,7 @@ export default function AboutMe(props) {
             <img src={require('./images/workplace-1246677_1920.jpg')}/>
             
             <div className={styles.text}>
-                <p>Hello. I am </p><a href="https://docs.google.com/document/d/1-a2JaiRNNneWVwWytYLipJjv3-YjRqu73BylTuNB6jM/edit?usp=sharing" target="_blank" className={styles.large} rel="noopener noreferrer">James.</a><p> I like building applications, learning new things and solving problems.</p>
+                <p>Hello. I am </p><a href={resume} target="_blank" className={styles.large} rel="noopener noreferrer">James.</a><p> I like building applications, learning new things and solving problems.</p>
                 
 
             </div>
--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './styles/contact.css';
+import resume from './assets/resume.pdf';
 
 export default function ContactForm(props) {
 
@@ -14,7 +15,7 @@ export default function ContactForm(props) {
                 <a href="mailto:jstuckey@rutgers.edu" className={styles.contact} target="_blank" rel="noopener noreferrer">jstuckey@rutgers.edu</a>
                 <a href="https://github.com/j-stuckey" className={styles.contact} target="_blank" rel="noopener noreferrer">Github</a>
                 <a href="https://www.linkedin.com/in/james-stuckey-5b1179b9/" className={styles.contact} target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                <a href="https://docs.google.com/document/d/1-a2JaiRNNneWVwWytYLipJjv3-YjRqu73BylTuNB6jM/edit?usp=sharing" className={styles.contact} target="_blank" rel="noopener noreferrer">Resume</a>
+                <a href={resume} className={styles.contact} target="_blank" rel="noopener noreferrer">Resume</a>
 
             </div>
         </section>
