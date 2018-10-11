db819c29f102b82db0c02edf73e29eb65b76b755
James Stuckey <jstuckey@rutgers.edu>
2018-10-10 23:59:56 -0400

    reformatted

--- src/App.js
+++ src/App.js
@@ -8,24 +8,19 @@ import { Route } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 class App extends Component {
-
-  render() {
-
-    return (
-        
-            <Router>
-                <React.Fragment>
-
-                    <Route path="/" component={NavBar} />
-                    <Route exact path="/" component={LandingPage} />
-                    <Route exact path="/projects" component={Projects}/>
-                    <Route exact path="/aboutme" component={AboutMe} />
-                    <Route exact path="/contact" component={ContactForm}/>
-                </React.Fragment>
-                
-            </Router>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<React.Fragment>
+					<Route path="/" component={NavBar} />
+					<Route exact path="/" component={LandingPage} />
+					<Route exact path="/projects" component={Projects} />
+					<Route exact path="/aboutme" component={AboutMe} />
+					<Route exact path="/contact" component={ContactForm} />
+				</React.Fragment>
+			</Router>
+		);
+	}
 }
 
 export default App;
