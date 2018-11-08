59d231a6189a5d6a8a543c10122f0e048f8b5a7e
James Stuckey <jstuckey@rutgers.edu>
2018-11-08 13:22:19 -0500

    added login form for blog with button, no functionality

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import styles from './styles/login.css';
 
-export default function Login(props) {
-	return <h1>hello world</h1>;
+export default class Login extends React.Component {
+	render() {
+		return (
+			<div className={styles.container}>
+				<label>Username</label>
+				<input type="text" className={styles.input} />
+
+				<label>Password</label>
+				<input type="text" className={styles.input} />
+
+				<button className={styles.button}>Log In</button>
+			</div>
+		);
+	}
 }
