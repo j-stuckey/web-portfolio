be971c8fa3d54beba1a0d0aaca6eb4e3df2de13c
James Stuckey <jstuckey@rutgers.edu>
2018-11-07 23:30:39 -0500

    removed about me components and styles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import NavBar from './nav';
 import Projects from './projects';
 import ContactForm from './contact';
 import LandingPage from './landing-page';
-import AboutMe from './aboutme';
 import { Route } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -15,7 +14,6 @@ class App extends Component {
 					<Route path="/" component={NavBar} />
 					<Route exact path="/" component={LandingPage} />
 					<Route exact path="/projects" component={Projects} />
-					<Route exact path="/aboutme" component={AboutMe} />
 					<Route exact path="/contact" component={ContactForm} />
 				</React.Fragment>
 			</Router>
