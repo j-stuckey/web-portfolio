d8eae84f9011eb112460dd42c38c40a7be4b7a13
James Stuckey <jstuckey@rutgers.edu>
2018-11-08 12:35:44 -0500

    added container for blog page

--- src/Blog.js
+++ src/Blog.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import styles from './styles/blog.css';
 
-export default class Blog extends React.Component {
+class Blog extends React.Component {
 	render() {
-		return <h1>Hello, World.</h1>;
+		return (
+			<div className={styles.container}>
+				<h1>Hello, World.</h1>
+			</div>
+		);
 	}
 }
+
+const mapStateToProps = state => {
+	return {};
+};
+
+export default connect(mapStateToProps)(Blog);
