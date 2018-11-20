cfac2e1f01d70169a51f0aa9d5928ba96cf3f61c
James Stuckey <jstuckey@rutgers.edu>
2018-11-19 23:31:45 -0500

    blog posts with RichTextEditor work

--- src/Blog-Form.js
+++ src/Blog-Form.js
@@ -5,38 +5,19 @@ import styles from './styles/blogform.module.css';
 import { connect } from 'react-redux';
 import { submitBlogPost } from './actions/blog';
 
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import { string } from 'prop-types';
+import RichTextEditor from 'react-rte';
 
 export class BlogForm extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			submitted: false,
-			text: ''
+			text: RichTextEditor.createEmptyValue(),
+			post: {}
 		};
 
 		this.title = React.createRef();
 		this.content = React.createRef();
-	}
-
-	submit(e) {
-		e.preventDefault();
-
-		// grabbing the post content using react refs
-		// wonder if there's a better way to do this.
-
-		const post = {
-			title: this.title.current.value,
-			content: this.content.current.value
-		};
-
-		this.setState({
-			submitted: true
-		});
-
-		this.props.dispatch(submitBlogPost(post));
 	}
 
 	handleChange(value) {
@@ -45,32 +26,80 @@ export class BlogForm extends React.Component {
 		});
 	}
 
+	onChange = value => {
+		this.setState({ text: value });
+
+		console.log(value.toString('html'));
+
+		const post = {
+			title: this.title.current.value,
+			content: value.toString('html')
+		};
+
+		this.setState({ post });
+
+		console.log(post);
+		if (this.props.onChange) {
+			// Send the changes up to the parent component as an HTML string.
+			// This is here to demonstrate using `.toString()` but in a real app it
+			// would be better to avoid generating a string on each change.
+			this.props.onChange(value.toString('html'));
+		}
+	};
+
+	submit(e, content) {
+		e.preventDefault();
+
+		// grabbing the post content using react refs
+		// wonder if there's a better way to do this.
+
+		const post = this.state.post;
+
+		this.setState({
+			submitted: true
+		});
+
+		this.props.dispatch(submitBlogPost(post));
+	}
+
 	render() {
 		if (this.state.submitted) {
 			return <Redirect to="/blog" />;
 		}
 
 		return (
-			<form onSubmit={e => this.submit(e)} className={styles.form}>
-				<h3>New Post</h3>
-				<label htmlFor="title">Title</label>
-				<input
-					name="title"
-					type="text"
-					ref={this.title}
-					className={styles.title}
-				/>
+			<React.Fragment>
+				<form
+					onSubmit={e => this.submit(e, this.content)}
+					// onSubmit={() => this.onChange()}
+					className={styles.form}
+				>
+					<h3>New Post</h3>
+					<label htmlFor="title">Title</label>
+					<input
+						name="title"
+						type="text"
+						ref={this.title}
+						className={styles.title}
+					/>
 
-				<label htmlFor="content">Content</label>
-				<textarea
-					name="content"
-					ref={this.content}
-					className={styles.content}
-				/>
+					<label htmlFor="content">Content</label>
+					{/* <textarea
+						name="content"
+						// ref={this.content}
+						className={styles.content}
+					/> */}
 
-				<button type="submit">Submit</button>
-				<a href="/blog">Back</a>
-			</form>
+					<RichTextEditor
+						className={styles.editor}
+						value={this.state.text}
+						onChange={this.onChange}
+						ref={this.content}
+					/>
+					<button type="submit">Submit</button>
+					<a href="/blog">Back</a>
+				</form>
+			</React.Fragment>
 		);
 	}
 }
