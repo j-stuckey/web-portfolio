039499dad64560a9799c5b407c22ded8b8d021e9
James Stuckey <jstuckey@rutgers.edu>
2019-03-01 14:09:51 -0500

    nothing

--- src/App.js
+++ src/App.js
@@ -13,55 +13,55 @@ import { refreshAuthToken } from './actions/auth';
 import BlogForm from './Blog-Form';
 
 class App extends Component {
-	componentDidUpdate(prevProps) {
-		if (!prevProps.loggedIn && this.props.loggedIn) {
-			this.startPeriodicRefresh();
-		}
-		if (prevProps.loggedIn && !this.props.loggedIn) {
-			this.stopPeriodicRefresh();
-		}
-	}
+    componentDidUpdate(prevProps) {
+        if (!prevProps.loggedIn && this.props.loggedIn) {
+            this.startPeriodicRefresh();
+        }
+        if (prevProps.loggedIn && !this.props.loggedIn) {
+            this.stopPeriodicRefresh();
+        }
+    }
 
-	componentWillMount() {
-		this.stopPeriodicRefresh();
-	}
+    componentWillMount() {
+        this.stopPeriodicRefresh();
+    }
 
-	startPeriodicRefresh() {
-		this.refreshInterval = setInterval(
-			() => this.props.dispatch(refreshAuthToken()),
-			// 15 minute refresh period
-			15 * 60 * 1000
-		);
-	}
+    startPeriodicRefresh() {
+        this.refreshInterval = setInterval(
+            () => this.props.dispatch(refreshAuthToken()),
+            // 15 minute refresh period
+            15 * 60 * 1000
+        );
+    }
 
-	stopPeriodicRefresh() {
-		if (!this.refreshInterval) {
-			return;
-		}
+    stopPeriodicRefresh() {
+        if (!this.refreshInterval) {
+            return;
+        }
 
-		clearInterval(this.refreshInterval);
-	}
+        clearInterval(this.refreshInterval);
+    }
 
-	render() {
-		return (
-			<Router>
-				<React.Fragment>
-					<Route path="/" component={NavBar} />
-					<Route exact path="/" component={LandingPage} />
-					<Route exact path="/projects" component={Projects} />
-					<Route exact path="/contact" component={ContactForm} />
-					<Route exact path="/blog" component={Blog} />
-					<Route exact path="/blog/new-post" component={BlogForm} />
-					<Route exact path="/login" component={LoginForm} />
-				</React.Fragment>
-			</Router>
-		);
-	}
+    render() {
+        return (
+            <Router>
+                <React.Fragment>
+                    <Route path="/" component={NavBar} />
+                    <Route exact path="/" component={LandingPage} />
+                    <Route exact path="/projects" component={Projects} />
+                    <Route exact path="/contact" component={ContactForm} />
+                    <Route exact path="/blog" component={Blog} />
+                    <Route exact path="/blog/new-post" component={BlogForm} />
+                    <Route exact path="/login" component={LoginForm} />
+                </React.Fragment>
+            </Router>
+        );
+    }
 }
 
 const mapStateToProps = state => ({
-	hasAuthToken: state.auth.authToken !== null,
-	loggedIn: state.auth.currentUser !== null
+    hasAuthToken: state.auth.authToken !== null,
+    loggedIn: state.auth.currentUser !== null
 });
 
 export default connect(mapStateToProps)(App);
